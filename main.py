f2c71e906731115f8e7a74c230c5b71c3c497598
Sarat Rajan <5793844+saratrajan@users.noreply.github.com>
2022-02-03 13:18:23 -0800

    Handling duplicate array elements

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 class Solution(object):
     def find_target_pairs(arr,target):
-        i = 0
-        j = len(arr)-1
+        i = 0 # First pointer
+        j = len(arr)-1 # Second pointer
         arr.sort()
         print(arr)
         while(i < j):
             diff = target - arr[j]
-            if(diff < 0):
+            if(diff < 0): #Eliminating the elements in array that are higher than target
                 print("Outer index is larger than the target")
                 j -= 1
             elif(arr[i] == diff):
                 print("[" + str(arr[j]) + " , " + str(arr[i]) + "]")
+                if(arr[i+1] == diff): # Additional logic to handle duplicates in sorted array
+                    print("[" + str(arr[j]) + " , " + str(arr[i+1]) + "]")
                 j -= 1
             elif(arr[i] > diff):
                 i += 1
@@ -18,7 +20,7 @@ class Solution(object):
             elif(arr[i] < diff):
                 i += 1
     
-    arr = [3,7,9,4,8,2,6,11,14,16,12]
+    arr = [3,7,9,4,8,2,6,11,14,16,12,3,12]
     target = 15
     
     find_target_pairs(arr,target)
